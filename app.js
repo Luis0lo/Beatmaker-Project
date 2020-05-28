43442a4faddfa12ed30c0579928583d02b5ccfe5
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2020-05-28 19:34:24 +0100

    sound

--- app.js
+++ app.js
@@ -16,7 +16,26 @@ class DrumKit {
   repeat() {
     let step = this.index % 8;
     const activeBars = document.querySelectorAll(`.b${step}`);
-    console.log(step);
+    ////////////////////////////////////// Loop over the pads
+    activeBars.forEach((bar) => {
+      bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+      ////////////////////////////////////Checking if pads are active
+      if (bar.classList.contains("active")) {
+        ///////////////////////////////////check each sound
+        if (bar.classList.contains("kick-pad")) {
+          this.kickAudio.currentTime = 0;
+          this.kickAudio.play();
+        }
+        if (bar.classList.contains("snare-pad")) {
+          this.snareAudio.currentTime = 0;
+          this.snareAudio.play();
+        }
+        if (bar.classList.contains("hihat-pad")) {
+          this.hihatAudio.currentTime = 0;
+          this.hihatAudio.play();
+        }
+      }
+    });
     this.index++;
   }
   start() {
@@ -31,6 +50,9 @@ const drumKit = new DrumKit();
 
 drumKit.pads.forEach((pad) => {
   pad.addEventListener("click", drumKit.activePad);
+  pad.addEventListener("animationend", function () {
+    this.style.animation = "";
+  });
 });
 ///////////////////////////////////////////////Adding Event to play button
 drumKit.playBtn.addEventListener("click", function () {
